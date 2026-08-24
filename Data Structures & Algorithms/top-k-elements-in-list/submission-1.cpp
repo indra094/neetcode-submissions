9be6cacc01168876ade8e9e05e1763df524f3c04
INDRAJEET NANDY <ij.nandy10@gmail.com>
2026-08-23 23:48:45 -0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,119 @@
+class Solution {
+public:
+
+   vector<int> findResult(vector<int>& nums, int k) {
+        unordered_map<int, int> counts;
+        for(int num: nums) {
+            counts[num]++;
+        }
+
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minHeap;
+        //1:3, 2:2, 3:1
+        for(const pair<int, int>& entry: counts) {
+            if(minHeap.size()==k && minHeap.top()[0]<entry.second) {
+                minHeap.pop();
+            }
+            if(minHeap.size()<k) {
+                minHeap.push({entry.second, entry.first});
+            }
+        }
+
+        vector<int> result;
+        while(!minHeap.empty()) {
+            result.push_back(minHeap.top()[1]);
+            minHeap.pop();
+        }
+        return result;
+    }
+
+    int partition(vector<int>& elems, const unordered_map<int, int>& counts, int left, int right) {
+        int pos = left;
+
+        int pivot = rand() % (right - left + 1) + left;
+        swap(elems[pivot], elems[right]);
+        // 1,2,3
+        for(int id=left; id<right; id++) {
+            if(counts.at(elems[id])>counts.at(elems[right])) {
+                swap(elems[id], elems[pos]);
+                pos++;
+                
+            }
+        }
+        //cout<<pos<<'\n';
+        swap(elems[pos], elems[right]);
+
+        return pos;
+    }
+
+    void quickSelect(vector<int>& elems, const unordered_map<int, int>& counts, int k) {
+        int left =0, right =elems.size()-1;
+        while(left<right) {
+            int pos = partition(elems, counts, left, right);
+            if(pos==k) {
+                break;
+            }
+            if (pos<k) {
+                left = pos+1;
+            }
+            else {
+                right = pos-1;
+            }
+        }
+    }
+
+    vector<int> findResult2(vector<int>& nums, int k) {
+        
+        vector<int> result;
+        unordered_map<int, int> counts;
+        for(int num:nums) {
+            counts[num]++;
+        }
+        vector<int> uniqueElems;
+        for(const auto& entry:counts) {
+            uniqueElems.push_back(entry.first);
+        }
+        if(k>=uniqueElems.size()) {
+            return uniqueElems;
+        }
+
+        quickSelect(uniqueElems, counts, k-1);
+        result.insert(result.end(), make_move_iterator(uniqueElems.begin()), make_move_iterator(uniqueElems.begin()+k));
+        return result;
+    }
+
+    vector<int> findResult3(vector<int>& nums, int k) {
+        vector<int> result;
+        unordered_map<int, int> counts;
+        unordered_map<int, vector<int> > buckets;
+        for(int num: nums) {
+            counts[num]++;
+        }
+        int maxFreq=0;
+        for(const auto& entry:counts) {
+            buckets[entry.second].push_back(entry.first);
+            maxFreq = max(maxFreq, entry.second);
+        }
+        for(int key=maxFreq; key>=0 && result.size()<k; key--) {
+            if(!buckets.contains(key)) {
+                continue;
+            }
+           // cout<<key<<buckets[key].size();
+            for(int num:buckets[key]) {
+                if(result.size()==k) {
+                    break;
+                }
+                result.push_back(num);
+            }
+        }
+        
+        return result;
+    }
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> result;
+        result = findResult3(nums, k);
+
+        return result;
+    }
+
+};
