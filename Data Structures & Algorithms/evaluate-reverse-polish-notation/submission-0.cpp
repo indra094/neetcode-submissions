5095a93cc4d0a7058b1b13ad585a0c0daf98dcb6
INDRAJEET NANDY <ij.nandy10@gmail.com>
2026-08-28 22:29:52 -0700

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+
+    bool isOp(const string& token) {
+        return (token=="-" || token=="*" || token == "+" || token=="/");        
+    }
+
+    int calculate(int var1, int var2, const string& op) {
+        if(op=="*") {
+            return var1*var2;
+        }
+        else if(op=="-"){
+            return var1-var2;
+        }
+        else if(op=="+"){
+            return var1+var2;
+        }
+        else {
+            return var1/var2;
+        }
+    }
+
+    int solve1(vector<string>& tokens) {
+        stack<int> vars;
+        for(auto& word:tokens) {
+            if(isOp(word)) {
+                int var2 = vars.top();
+                vars.pop();
+                int var1 = vars.top();
+                vars.top() = calculate(var1, var2, word);
+                continue;
+            }
+            vars.push(stoi(word));
+        }
+        return vars.top();
+    }
+
+    int solve2(vector<string>& tokens) {
+        int var1=-2, var2=-1;
+        for(int op=0; op<tokens.size(); op++) {
+            if(isOp(tokens[op])) {
+                var2--, var1--;
+                tokens[var2] = to_string(calculate(stoi(tokens[var1]), stoi(tokens[var2]), tokens[op]));
+            }
+            else {
+                var1++,var2++;
+                tokens[var2]=tokens[op];
+            }
+        }
+        cout<<var2;
+        return stoi(tokens[var2]);
+    }
+
+    int evalRPN(vector<string>& tokens) {
+        return solve1(tokens);
+        return solve2(tokens);
+    }
+};
